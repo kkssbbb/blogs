8ddd8c2735f32fcbd1a1ba756b44722a32cc5463
KING_MASS <03337@naver.com>
2022-02-16 09:31:07 +0900

    환경세팅

---
+++ src/main/java/com/kkssbbb/blogs/BlogsApplication.java
@@ -0,0 +1,15 @@
+package com.kkssbbb.blogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+public class BlogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogsApplication.class, args);
+    }
+
+}
