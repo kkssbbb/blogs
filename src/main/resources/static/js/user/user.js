ee0da813abb531d7022f1f08f99022b1a81d6511
KING_MASS <03337@naver.com>
2022-02-26 14:53:18 +0900

    37.  joinForm.mustache 에 user.js 참조 설정까지 경로설정삽질로 혹시몰라서 깃헙에 푸쉬2

--- src/main/resources/static/js/user/user.js
+++ src/main/resources/static/js/user/user.js
@@ -6,8 +6,16 @@ let index={
     },
      //위의 세이브 버튼을 클릭하면 밑의 세이브함수가 호출된다
      save: function (){
-         alert('user의 save함수 호출됨');
+         //alert('user의 save함수 호출됨');
+         let data ={
+             username: $("#username").val(),
+             password: $("#password").val(),
+             email: $("#email").val()
+         }
+         //console.log(data);
+         $.ajax().done().fail(); //ajax 통신을 이용해서 3개의 데이터를 json 으로 변경하여 insert 요청
      }
 }
-
+//회원 가입시 Ajax를 사용하는 2가지 이유
+//1. 요청에 대한 응답을 html 이 아닌 Data(json)을 받기위해서
 index.init();
